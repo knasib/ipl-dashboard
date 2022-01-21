e61ec3437ca705b213aa047d9c763f46eee63749
nasib.sahu <nasib.sahu@asg.com>
2022-01-22 02:37:55 +0530

    Build related changes and few bug fixes

--- build-script.sh
+++ build-script.sh
@@ -5,7 +5,6 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-
 # Build the frontend project and copy the dist files into ipl-data-provider\src\main\resources\static
 cd frontend/ipl-dashboard-app
 ng b --configuration production
@@ -15,7 +14,6 @@ cp -r ./dist/ipl-dashboard-app/* ../../ipl-data-provider/src/main/resources/stat
 # Build the Backend project
 cd ../../
 mvn clean install -DskipTests=true
-
 
 # Update the Docker tag and push to the repository
 docker login -u $1 -p $2
